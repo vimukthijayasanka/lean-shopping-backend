0780cabc381cf847fb603e8e54599773daf5cb80
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-05-13 13:00:50 +0530

    Initial commit

---
+++ src/main/java/lki/ijse/dep13/leanbackend/LeanClothingApplication.java
@@ -0,0 +1,13 @@
+package lki.ijse.dep13.leanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanClothingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanClothingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lki/ijse/dep13/leanbackend/LeanClothingApplicationTests.java
@@ -0,0 +1,13 @@
+package lki.ijse.dep13.leanbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanClothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
